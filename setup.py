2c53929a0100c183f3a3f00173735ade2334f43f
Tarang Varshneya <tarangvarshneya1@gmail.com>
2025-06-22 15:40:22 +0530

    folder structure edit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="generative AI Project",
+    version="0.0.0",
+    author="Tarang Varshney",
+    author_email="tarangvarshneya1@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
